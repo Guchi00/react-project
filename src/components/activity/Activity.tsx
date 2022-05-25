db80705910e439d076b5545d316fec5f07dc398f
Emmanuel Ihemegbulam <emmanuelroic@gmail.com>
2022-05-25 08:49:43 +0100

    implemented collapse/expand

--- src/components/activity/Activity.tsx
+++ src/components/activity/Activity.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 interface ActivityType {
@@ -8,14 +8,33 @@ interface ActivityType {
 }
 
 function Activity({message, createdAt, author}: ActivityType) {
+  const [bottomClass, setBottomClass] = useState("bottom hide");
+  const [buttonText, setButtonText] = useState("Expand");
+
+  const toggleBottom = () => {
+    if (bottomClass === "bottom") {
+      setBottomClass("bottom hide");
+      setButtonText("Expand")
+    } else {
+      setBottomClass("bottom");
+      setButtonText("Collapse");
+    }
+  };
+  
   return (
     <div className="expand">
-      <h3 className="h3">
-        {message}
-      </h3>
-      <button className="h3" type="button">
-        Expand
-      </button>
+      <div className="top">
+        <h3 className="h3">
+          {message}
+        </h3>
+        <button className="h3" type="button" onClick={toggleBottom}>
+          {buttonText}
+        </button>
+      </div>
+      <div className={bottomClass}>
+        <span className="time">{createdAt}</span>
+        <span className="author">{author}</span>
+      </div>
     </div>
   );
 }
